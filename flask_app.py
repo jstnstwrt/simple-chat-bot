0ba02dc3e1588e61a1ec6d3e5dd91fbfdd2715e6
Justin Stewart <justinstewart.la@gmail.com>
2017-03-22 19:49:43 -0400

    added apiai

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,33 @@
+import apiai
+import json
 from flask import Flask
+
+from access_tokens import client_access_token
+
+ai = apiai.ApiAI(client_access_token)
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route("/test")
 def hello():
     return "Hello World!"
 
-
 @app.route("/<msg>", methods=['GET', 'POST'])
 def server(msg):
-    return str(msg)
+
+    # prepare API.ai request
+    req = ai.text_request()
+    req.lang = 'en'  # optional, default value equal 'en'
+    req.query = msg
+
+     # get response from API.ai
+    api_response = req.getresponse()
+    responsestr = api_response.read().decode('utf-8')
+    response_obj = json.loads(responsestr)
+
+    if 'result' in response_obj:
+        response = response_obj["result"]["fulfillment"]["speech"]
+
+    return str(response)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
