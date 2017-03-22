3af6646edb8831ce8eda77214b8e2159e25785e0
Hugo Bowne-Anderson <hugobowne@gmail.com>
2017-03-22 19:42:04 -0400

    first chatbot

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,35 @@
+import apiai
+
+# api.ai account info
+CLIENT_ACCESS_TOKEN = "78c0e0________________fbcd9404a2"
+ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/hello')
+def hello_world():
+    return 'Hello api.ai (from Flask!)'
+
+@app.route("/<msg>", methods=['GET', 'POST'])
+def server(msg):
+
+    # prepare API.ai request
+    req = ai.text_request()
+    req.lang = 'en'  # optional, default value equal 'en'
+    req.query = msg
+
+    # get response from API.ai
+    api_response = req.getresponse()
+    responsestr = api_response.read().decode('utf-8')
+    response_obj = json.loads(responsestr)
+    if 'result' in response_obj:
+        response = response_obj["result"]["fulfillment"]["speech"]
+
+        return response
+
+    else
+        return 'I do not understand.'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
