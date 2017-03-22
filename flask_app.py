8b36fd2d2478cf0b17eef5ff295938efb9d08057
Justin Stewart <justinstewart.la@gmail.com>
2017-03-22 19:36:23 -0400

    added route

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello():
     return "Hello World!"
+
+
+@app.route("/<msg>", methods=['GET', 'POST'])
+def server(msg):
+    return str(msg)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
