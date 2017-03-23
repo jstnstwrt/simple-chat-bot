717ce9fcc4b54ba058cc959ec2b45adaab915974
Justin Stewart <justinstewart.la@gmail.com>
2017-03-22 21:07:14 -0400

    testing twilio

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,12 @@
-import apiai
 import json
-from flask import Flask
-
+from flask import Flask,request
+import apiai
+import twilio.twiml
+from twilio.rest import TwilioRestClient
 import config
 
+# Configurations for twilio, apiai, and flask.
+client = TwilioRestClient(config.account_sid, config.auth_token)
 ai = apiai.ApiAI(config.client_access_token)
 app = Flask(__name__)
 
@@ -11,23 +14,30 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.route("/<msg>", methods=['GET', 'POST'])
-def server(msg):
+@app.route("/", methods=['GET', 'POST'])
+def server():
 
-    # prepare API.ai request
-    req = ai.text_request()
-    req.lang = 'en'  # optional, default value equal 'en'
-    req.query = msg
+    # get SMS input via twilio
+    resp = twilio.twiml.Response()
 
-     # get response from API.ai
-    api_response = req.getresponse()
-    responsestr = api_response.read().decode('utf-8')
-    response_obj = json.loads(responsestr)
+    # get SMS metadata
+    msg_from = request.values.get("From", None)
+    msg = request.values.get("Body", None)
 
-    if 'result' in response_obj:
-        response = response_obj["result"]["fulfillment"]["speech"]
+    # # prepare API.ai request
+    # req = ai.text_request()
+    # req.lang = 'en'  # optional, default value equal 'en'
+    # req.query = msg
 
-    return str(response)
+    #  # get response from API.ai
+    # api_response = req.getresponse()
+    # responsestr = api_response.read().decode('utf-8')
+    # response_obj = json.loads(responsestr)
+
+    # if 'result' in response_obj:
+    #     response = response_obj["result"]["fulfillment"]["speech"]
+
+    return str(msg_from)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
